78e549410925fdbad1ce6c361e6d017f4c77355d
Mohamed Gad <gadm5336@gmail.com>
2026-01-06 14:10:36 -0600

    Added arithmetic basics logic

---
+++ 01_basics/07_arithmetic_basics.py
@@ -0,0 +1,15 @@
+budget = float(input("Enter your starting budget: "))
+
+print(f"Starting budget: {budget}")
+
+budget = budget - 550
+print(f"After spending 550 on the car loan: {budget}")
+
+budget = budget + 150
+print(f"After earning 150 from freelancing: {budget}")
+
+budget = budget - 30
+print(f"After spending 30 on gas: {budget}")
+
+budget = budget - 150
+print(f"After spending 150 on insurance: {budget}")
