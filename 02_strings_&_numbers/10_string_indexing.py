1d12b1b0a89449529d075bbcc850e2fc3fd9a68d
Mohamed Gad <gadm5336@gmail.com>
2026-01-06 14:34:50 -0600

    Added string indexing logic

---
+++ 02_strings_&_numbers/10_string_indexing.py
@@ -0,0 +1,13 @@
+user_input = input("enter a 6 letter word: ")
+
+print(f"Original word: {user_input}")
+
+print(f"First Character: {user_input[0]}")
+
+print(f"Last Character: {user_input[-1]}")
+
+print(f"First three Characters: {user_input[0:3]}")
+
+print(f"last three Characters: {user_input[3:]}")
+
+print(f"All letters between first and last letters: {user_input[1:-1]}")
