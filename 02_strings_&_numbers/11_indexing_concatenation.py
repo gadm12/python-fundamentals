4f51436e4bc2667f2bd6531cc16fdc30df7f2984
Mohamed Gad <gadm5336@gmail.com>
2026-01-06 16:49:21 -0600

    Added indexing concatenation logic

---
+++ 02_strings_&_numbers/11_indexing_concatenation.py
@@ -0,0 +1,14 @@
+sentence = "Well, I truly love pancakes, yet the python escaped."
+
+
+word1 = sentence[6]
+print(f"word1: {word1}")
+
+word2 = sentence[14:18]
+print(f"word1: {word2}")
+
+word3 = sentence[37:43]
+print(f"word1: {word3}")
+
+final_message = word1 + " " + word2 + " " + word3
+print(f"combined_sentence: {final_message}")
