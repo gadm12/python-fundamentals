484659d78737c31045f7218fc2c6351f590c3a17
Mohamed Gad <gadm5336@gmail.com>
2026-01-06 14:23:46 -0600

    Added simple indexing logic

---
+++ 02_strings_&_numbers/09_simple_indexing.py
@@ -0,0 +1,11 @@
+word = "Apple"
+
+print(f"word: {word}")
+
+print(f"First Character: {word[0]}")
+
+print(f"Last Character: {word[0]}")
+
+middle_char = len(word) // 2
+
+print(f"Middle Character: {word[middle_char]}")
