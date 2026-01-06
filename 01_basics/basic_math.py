606577b7f2cbdc166aa95ed6bee79969b8678efc
Mohamed Gad <gadm5336@gmail.com>
2026-01-06 13:54:37 -0600

    Added basic math logic

---
+++ 01_basics/basic_math.py
@@ -0,0 +1,7 @@
+english = int(input("enter english grade: "))
+math = int(input("enter math grade: "))
+history = int(input("enter history grade: "))
+
+total = english + math + history
+sum = total / 3
+print(sum)
