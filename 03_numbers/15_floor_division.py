9b30e338e382f30f13614c901bd8a29d2712327a
Mohamed Gad <gadm5336@gmail.com>
2026-01-07 06:04:20 -0600

    Added floor division logic

---
+++ 03_numbers/15_floor_division.py
@@ -0,0 +1,9 @@
+number_of_students = 23
+students_per_team = 4
+
+number_of_teams = number_of_students // students_per_team
+
+print(f"Number of students: {number_of_students}")
+print(f"Number of students per team: {students_per_team}")
+
+print(f"How many teams: {number_of_teams}")
