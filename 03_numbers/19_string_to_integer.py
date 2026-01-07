68368c14e7e55990b5a11ff92c18d49e5f4954cc
Mohamed Gad <gadm5336@gmail.com>
2026-01-07 06:29:58 -0600

    Added string to integer logic

---
+++ 03_numbers/19_string_to_integer.py
@@ -0,0 +1,11 @@
+num1 = input("enter the first number: ")
+
+num2 = input("enter the second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+sum = num1 + num2
+num1 = str(num1)
+num2 = str(num2)
+sum = str(sum)
+print(f"sum: {sum}")
