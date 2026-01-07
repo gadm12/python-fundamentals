2c3b4713801ab8b8311031248f49840daa3c995c
Mohamed Gad <gadm5336@gmail.com>
2026-01-07 05:20:17 -0600

    Added multiplication logic

---
+++ 03_numbers/14_muliplication.py
@@ -0,0 +1,7 @@
+pencils_per_box = 20
+number_of_boxes = 4
+
+print(f"Number of pencils: {pencils_per_box}")
+print(f"Number of boxes: {number_of_boxes}")
+total = pencils_per_box * number_of_boxes
+print(f"The total number of pencils: {total}")
