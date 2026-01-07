0f08e081e24315937686c84aafe12904c78d3f6a
Mohamed Gad <gadm5336@gmail.com>
2026-01-07 03:56:55 -0600

    Added string capitalization logic

---
+++ 02_strings_&_numbers/12_string_capitalization.py
@@ -0,0 +1,10 @@
+text = "learning Python is FUN!"
+
+text = text.lower()
+print(f"Lowercase: {text}")
+
+text = text.upper()
+print(f"Uppercase: {text}")
+
+text = text.capitalize()
+print(f"Capitalized: {text}")
