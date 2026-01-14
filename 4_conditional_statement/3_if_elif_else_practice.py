4c815dd63fd80616f75a75f56b778314f1c2d709
Mohamed Gad <gadm5336@gmail.com>
2026-01-14 16:30:38 -0600

    Added if elif else logic

---
+++ 4_conditional_statement/3_if_elif_else_practice.py
@@ -0,0 +1,9 @@
+enemy_level = 7
+
+
+if enemy_level >= 1 and enemy_level <= 3:
+    print("Minor treat")
+elif enemy_level >= 4 and enemy_level <= 6:
+    print("Moderate treat")
+else:
+    print("Severe treat")
