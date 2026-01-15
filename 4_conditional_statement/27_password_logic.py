92923140e0a09aa5fde4ed5555cad7aa4143977c
Mohamed Gad <gadm5336@gmail.com>
2026-01-15 07:45:54 -0600

    Added password logic

---
+++ 4_conditional_statement/27_password_logic.py
@@ -0,0 +1,11 @@
+password = input("password: ")
+
+if password == "OpenSesame":
+    print("The door is opening")
+elif password == "Friend":
+    print("The door opens silently")
+elif password == "swordfish":
+    print("The door opens, and there was a loud click")
+
+else:
+    print("nothing happens!")
