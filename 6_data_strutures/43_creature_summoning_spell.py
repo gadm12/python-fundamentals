e3c0c7a301cc94e49e9e3e9211ea6033b35827b8
Mohamed Gad <gadm5336@gmail.com>
2026-01-18 09:08:36 -0600

    Added creature summoning

---
+++ 6_data_strutures/43_creature_summoning_spell.py
@@ -0,0 +1,5 @@
+creatures = ["Goblin", "Orc", "Troll", "Dragon", "Phoenix"]
+
+print(f"First creature: {creatures[0]}")
+print(f"Middle creature: {creatures[len(creatures)//2]}")
+print(f"Last creature: {creatures[-1]}")
