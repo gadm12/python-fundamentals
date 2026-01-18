d76664ffdda3d4a974fd5f5996aa8a694b69e245
Mohamed Gad <gadm5336@gmail.com>
2026-01-18 12:52:36 -0600

    Added update multiple stats

---
+++ 6_data_strutures/50_update_multiple_stats.py
@@ -0,0 +1,6 @@
+character = {"name": "Thorn", "level": 2, "health": 20}
+
+character["level"] += 1
+character["health"] += 10
+print(f"{character['name']}'s new level: {character['level']}")
+print(f"{character['name']}'s new health: {character['health']}")
