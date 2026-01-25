2910ec44cddfcd802397fd9915d27c663a308520
Mohamed Gad <gadm5336@gmail.com>
2026-01-25 10:32:16 -0600

    Added calculate strength

---
+++ 7_functions/58_calculating_strength.py
@@ -0,0 +1,10 @@
+def calculate_strength(level):
+    strength = level * 10
+    return strength
+
+
+print(calculate_strength(3))
+
+char_str = calculate_strength(3)
+print(char_str)
+print(type(char_str))
